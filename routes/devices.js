827e5ba1e959c7bfb859387a937f16364d008978
Матвей Калинин <matvey.kalinin2001@gmail.com>
2021-12-17 15:00:26 +0300

    создание оборудования POST запросом

--- routes/devices.js
+++ routes/devices.js
@@ -3,10 +3,23 @@ const dbo = require('../db');
 
 let router = express.Router();
 
+class Device {
+	ip = "";
+	mac = "";
+	constructor(name, ports, position) {
+		this.name = name;
+		this.ports = ports;
+		this.position = position;
+	}
+}
+
 // GET /api/forge/oauth/token - generates a public access token (required by the Forge viewer).
 router.post('', async (req, res, next) => {
 	const db = dbo.getDb();
-	const device = { name: 'router' };
+	const { name, position, ip, mac, ports } = req.body;
+	const device = new Device(name, ports, position);
+	device.ip = ip;
+	device.mac = mac;
 	db.collection('devices').insertOne(device, (err, result) => {
 		if (err) res.status(400).send(err);
 		else res.send(result);
