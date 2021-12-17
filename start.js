87752c4cbee0459d55945a3fb0258b74d2eba129
Матвей Калинин <matvey.kalinin2001@gmail.com>
2021-12-17 14:09:30 +0300

    add mongodb

--- start.js
+++ start.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const express = require('express');
-
+const bodyParser = require('body-parser');
 require('dotenv').config()
+const db = require('./db');
+
 const PORT = process.env.PORT || 3000;
 const config = require('./config');
 if (config.credentials.client_id == null || config.credentials.client_secret == null) {
@@ -12,11 +14,17 @@ if (config.credentials.client_id == null || config.credentials.client_secret == 
 let app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api/forge/oauth', require('./routes/oauth'));
 app.use('/api/forge/oss', require('./routes/oss'));
 app.use('/api/forge/modelderivative', require('./routes/modelderivative'));
+app.use('/api/v1/devices', require('./routes/devices'))
 app.use((err, req, res, next) => {
     console.error(err);
     res.status(err.statusCode).json(err);
 });
-app.listen(PORT, () => { console.log(`Server listening on port http://localhost:${PORT} bruh`); });
+db.connectToServer(err => {
+    if (err)
+        return;
+    app.listen(PORT, () => { console.log(`Server listening on port http://localhost:${PORT} bruh`); });
+})
